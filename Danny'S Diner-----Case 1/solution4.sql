f3ec4ac5a028bf1abdcd81b91b2b7353b6205bd9
shashank kumar <shashank199929@gmail.com>
2023-03-22 01:32:34 +0530

    changes

--- Danny'S Diner-----Case 1/solution4.sql
+++ Danny'S Diner-----Case 1/solution4.sql
@@ -4,3 +4,13 @@ product_name from sales as s join menu as m
 on s.product_id=m.product_id
 group by product_name
 order by number_of_times desc;
+
+--Answer:
+--| most_purchased | product_name | 
+--| ----------- | ----------- |
+--| 8       | ramen |
+
+
+--- Most purchased item on the menu is ramen which is 8 times. Yummy!
+
+--
