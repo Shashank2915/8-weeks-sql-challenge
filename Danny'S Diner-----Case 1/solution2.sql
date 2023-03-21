26578bd8e7c04d5810560e0ecaf436d6b818fa30
shashank kumar <shashank199929@gmail.com>
2023-03-22 01:30:26 +0530

    changes merged

--- Danny'S Diner-----Case 1/solution2.sql
+++ Danny'S Diner-----Case 1/solution2.sql
@@ -2,3 +2,16 @@
 SELECT customer_id, COUNT(DISTINCT(order_date)) AS visit_count
 FROM sales
 GROUP BY customer_id;
+
+--Answer:
+--| customer_id | visit_count |
+--| ----------- | ----------- |
+--| A           | 4          |
+--| B           | 6          |
+--| C           | 2          |
+
+--- Customer A visited 4 times.
+--- Customer B visited 6 times.
+--- Customer C visited 2 times.
+
+---
