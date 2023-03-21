1dcf15ff078f611e63942490cab9c5005c8b5d7e
shashank kumar <shashank199929@gmail.com>
2023-03-21 14:51:37 +0530

    changes merged

--- Danny'S Diner-----Case 1/solution8.sql
+++ Danny'S Diner-----Case 1/solution8.sql
@@ -1,6 +1,6 @@
 -- 8. What is the total items and amount spent for each member before they became a member ?
 select s.customer_id,
-COUNT(distinct m.product_id) as COUNT, sum(m.price) as price
+COUNT(distinct m.product_id) as COUNT, sum(m.price) as PRICE
 from sales as s 
 join menu as m
 on s.product_id=m.product_id
